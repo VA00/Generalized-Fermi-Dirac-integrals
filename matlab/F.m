7e3992e4aab1a7a9f1efe4e2cbe3be7501323342
VA00 <aodrzywolek@gmail.com>
2021-11-21 19:40:06 +0100

    MATLAB integral tolerance experiments

--- matlab/F.m
+++ matlab/F.m
@@ -2,5 +2,5 @@ function y=F(k,eta,theta)
 %Calculation using the integral representation 
 %A.Gil J.Segura N.M.Temme https://www.sciencedirect.com/science/article/pii/S0096300321007025
 integrand=@(x)x.^k.*sqrt(1+theta*x/2)./(1+exp(x-eta));
-y=integral(integrand,0,inf,'RelTol',4.4e-16,'Waypoints',[eta]);
- 
+y=integral(integrand,0,inf,'RelTol',1.1e-16,'Waypoints',[eta]);
+end
