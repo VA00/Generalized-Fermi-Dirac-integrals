303a8a483ddc4a72fc10bc5af6d651f98e049c0e
VA00 <aodrzywolek@gmail.com>
2021-11-21 09:39:55 +0100

    Reference to Gil et. al. added

--- matlab/F.m
+++ matlab/F.m
@@ -1,5 +1,6 @@
 function y=F(k,eta,theta) 
-%Calculation using the integral representation
+%Calculation using the integral representation 
+%A.Gil J.Segura N.M.Temme https://www.sciencedirect.com/science/article/pii/S0096300321007025
 integrand=@(x)x.^k.*sqrt(1+theta*x/2)./(1+exp(x-eta));
 y=integral(integrand,0,inf,'RelTol',4.4e-16,'Waypoints',[eta]);
  
