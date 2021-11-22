8aea74645e35b243a4b0419c7418e21f4f4da951
VA00 <aodrzywolek@gmail.com>
2021-11-22 21:50:19 +0100

    Added FXT dfermi to comparison

--- tests/continuity_test.c
+++ tests/continuity_test.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <float.h>
-
+void dfermi_(double *, double *,double *,double *,double *,double *,double *,double *,double *);
 
 int main()
 {
@@ -10,10 +10,15 @@ int main()
   int i,j,m,n;
   double s, lead[DERIVATIVE_MATRIX_SIZE],D[DERIVATIVE_MATRIX_SIZE][DERIVATIVE_MATRIX_SIZE];
   double result[10];
-  double t=0.0, k=1.0, eta=2222.0, theta=4.0, z;
-  double split_point=65536.0;
+  double t=0.0, k=1.0, eta, theta=4.0, z;
+  double split_point=8192.0;
   double nearby[256];
+    double F, dF_deta, d2F_deta2, 
+		 dF_dtheta, d2F_dtheta2, d2F_deta_dtheta,
+		 d3F_dtheta3, d3F_dtheta2_deta, d3F_dtheta_deta2, d3F_deta3;
 
+
+   
   eta=split_point;
   for(i=128;i<256;i++)
    {
@@ -21,9 +26,7 @@ int main()
      eta=nextafter(eta,DBL_MAX);
    }
    
-
   eta=split_point; 
-
   for(i=128;i>=0;i--)
    {
      nearby[i]=eta;
@@ -36,13 +39,19 @@ int main()
 
   
   for(i=0;i<256;i++)
-   {   Ffermi_derivatives_matrix(k,nearby[i],theta,D);
+   {   
+       eta = nearby[i];
+       //dfermi_(&k,&eta,&theta, &F, &dF_deta, &dF_dtheta, &d2F_deta2, &d2F_dtheta2, &d2F_deta_dtheta);
+       //D[0][0]=F; D[0][1] = dF_dtheta; D[0][2] = d2F_dtheta2; D[1][0] = dF_deta; D[1][1] = d2F_deta_dtheta; D[2][0] = d2F_deta2;
+       Ffermi_derivatives_matrix(k,nearby[i],theta,D);
        for(m=0;m<DERIVATIVE_MATRIX_SIZE;m++)
         for(n=0;n<DERIVATIVE_MATRIX_SIZE;n++)
          if(m+n<=3)
            printf("%.17e\t", D[m][n]);
          else
           continue;
+    printf("%d\t", i-128);
+    printf("%.17e\t", nearby[i]);
     printf("\n");
    }
   
