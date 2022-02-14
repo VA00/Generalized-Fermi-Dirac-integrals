b897afc84cdd29b85210d06cbf2633575fb7f3ac
VA00 <aodrzywolek@gmail.com>
2022-02-14 21:11:12 +0100

    Precision increased to 1024 digits, due to still present wrong results

--- tests/refVALS/k_slices/0.5/refVALS.m
+++ tests/refVALS/k_slices/0.5/refVALS.m
@@ -1,7 +1,7 @@
 (* Andrzej Odrzywołek, andrzej.odrzywolek@uj.edu.pl, 9 Feb 2022 *)
 (* Reference value generator for generalized relativistic Fermi-Dirac integrals (GFDI) *)
 $IgnoreEOF=1;
-$MaxExtraPrecision = 1024;
+$MaxExtraPrecision = 4096;
 (* Main function *)
 F[k_, \[Eta]_, \[Theta]_, m_, n_] := Module[{integrand, f, x},
   f = Function[{z, kk, \[Eta]\[Eta], \[Theta]\[Theta]}, 
@@ -12,11 +12,11 @@ F[k_, \[Eta]_, \[Theta]_, m_, n_] := Module[{integrand, f, x},
     Check[
       NIntegrate[Evaluate@integrand, {x, 0, \[Eta]}, 
        Method -> "GlobalAdaptive", MinRecursion -> 13, 
-       MaxRecursion -> 64, WorkingPrecision -> 64], Infinity] +
+       MaxRecursion -> 64, WorkingPrecision -> 1024, PrecisionGoal->64], Infinity] +
      Check[
       NIntegrate[Evaluate@integrand, {x, \[Eta], Infinity}, 
        Method -> "GlobalAdaptive", MinRecursion -> 11, 
-       MaxRecursion -> 29, WorkingPrecision -> 64], Infinity],
+       MaxRecursion -> 29, WorkingPrecision -> 1024, PrecisionGoal->64], Infinity],
     Check[
      NIntegrate[Evaluate@integrand, {x, 0, Infinity}, 
       WorkingPrecision -> 64, Method -> "DoubleExponential", 
@@ -25,13 +25,13 @@ F[k_, \[Eta]_, \[Theta]_, m_, n_] := Module[{integrand, f, x},
   ];
 (* MAIN reference values generator. Beware of Mathematica BUGS !!!!! *)
 (* kTBL = Join[{-1/2, 0}, Table[2^i, {i, -1, 6}]];*)
-delta=11;
-kTBL={1/2};
-\[Theta]TBL = Join[{0}, Table[2^i, {i, -1022, 1024, delta}]];
+delta=3;
+kTBL={1};
+\[Theta]TBL = Join[{0}, Table[2^i, {i, -1022, 1024, delta}]]; (* MIN -1022 replaced by -1 *)
 (* \[Theta]TBL = {1}; *)
 \[Eta]TBL = 
-  Join[Table[-2^i, {i, 1024, -1022, -delta}], {0}, 
-   Table[2^i, {i, -1022, 26, delta}]];
+  Join[Table[-2^i, {i, 1024, -1, -delta}], {0}, 
+   Table[2^i, {i, -1, 26, delta}]];
 (*\[Eta]TBL=SetPrecision[Import["H:\\Fermi-Dirac\\Generalized-Fermi-Dirac-integrals\\tests\\continuity.dat","TSV"][[1;;,-2]],64];*)
 inputTBLall=Flatten[Table[{k,\[Eta],\[Theta]},{k,kTBL},{\[Eta],\[Eta]TBL},{\[Theta],\[Theta]TBL}],2];
 inputTBLall//Length
@@ -45,12 +45,15 @@ timerStart=AbsoluteTime[];
 mma3=ParallelTable[Table[If[m+n<=3,F[inputTBL[[ii,1]],inputTBL[[ii,2]],inputTBL[[ii,3]],m,n],Nothing],{m,0,3},{n,0,3}]//Flatten,{ii,1,Length[inputTBL]}];
 time3=AbsoluteTime[]-timerStart
 refTBLtmp=Join[Transpose[inputTBL],Transpose[mma3]]//Transpose;
-refTBL=Select[refTBLtmp, NumericQ[Total[#]] &];
+refTBL=Select[refTBLtmp, NumericQ[Total[#]] && Precision[Total[#]]>32 &];
 Export["refTBL_double_"<>DateString["ISODate"]<>".bin",refTBL,"Real64"]
 Export["refTBL_quad_"<>DateString["ISODate"]<>".bin",refTBL,"Real128"]
 Export["Mma_good_REF_finally_"<>DateString["ISODate"]<>".m",refTBL]
 Print["Total reference values:\t",Length[refTBL]];
 Print["Total time:\t",time3];
 Print[DateString[]];
+Print[inputTBLall//Length]
+Print[Length[inputTBL]]
+Print[Length[refTBL]]
 
 
